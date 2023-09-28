beb07365706deadcc9b4f1923e0581c9fd2a0a4f
IBS-AFICIONADO <j.dezwart@student.utwente.nl>
2023-09-28 15:53:22 +0200

    changed vision to raycasting to account for objects

--- Whitebox setup/Assets/Scripts/RobotManager.cs
+++ Whitebox setup/Assets/Scripts/RobotManager.cs
@@ -10,76 +10,117 @@ public enum AlertStage
     Alerted
 
 }
+
 public class RobotManager : MonoBehaviour
 {
-    public float fov;
-    public AlertStage alertStage;
-    const float maxAlert = 100f;
+    //parameters vision
+    public float radius;
     [Range(0, 360)] public float fovAngle;
+
+    //parameters before enum change
+    const float maxAlert = 100f;
+    public float alertSpeed = 1;
+    public float forgetSpeed = 1;
+    
+
+    public AlertStage alertStage;
     [Range(0, maxAlert)] public float alertLevel;
-    public float maximumAlert = maxAlert;
 
-    public GameObject alertUI;
-    public Slider alertSlider;
-    public Image alertSliderFill;
+    //parametrs for raycasting, detecting objects
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    
+  
 
+    //parameters for changing UI above head enemies
+    //public GameObject alertUI;
+   // public Slider alertSlider;
+   // public Image alertSliderFill;
+
+    //parameters enemy movement
     public float turnRate;
     public float speed;
 
+    //parameters needed in the editor but not important fror changing in editor
+    [HideInInspector]
+    public bool playerInFOV;
+    [HideInInspector]
+    public float maxAlertEdit = maxAlert;
+    [HideInInspector]
+    public GameObject targetRef;
     private void Awake()
     {
         alertStage = AlertStage.Peaceful;
         alertLevel = 0;
-        alertSlider.value = 0;
+     //   alertSlider.value = 0;
+        targetRef = GameObject.FindGameObjectWithTag("Player");
     }
+
 
     private void Update()
     {
-        bool playerInFOV = false;
-        Collider[] targetsInFOV = Physics.OverlapSphere(
-            transform.position, fov);
-        
-        foreach(Collider c in targetsInFOV)
-        {
-            if (c.CompareTag("Player"))
-            {
-                float signedAngle = Vector3.Angle(transform.forward, c.transform.position - transform.position);
-                if (Mathf.Abs(signedAngle) < fovAngle/2)
-                    playerInFOV = true;
-                break;
-            }
-        }
-
-        Alertslider();
-        UpdateAlertstate(playerInFOV);
-        RobotMovement(targetsInFOV);
+        StartCoroutine(FOVRoutine());
  
     }
 
-    private void Alertslider()
+    private IEnumerator FOVRoutine()
     {
-        alertUI.SetActive(false);
-        if (alertLevel != 0)
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+
+        while (true)
         {
-            alertUI.SetActive(true);
-            alertSlider.value = alertLevel / maxAlert;
-            alertSliderFill.color = Color.Lerp(Color.yellow, Color.red, alertSlider.value);
+            yield return wait;
+            FOVCheck();
+            UpdateAlertstate(playerInFOV);
+         
+           RobotMovement(targetRef);
+
         }
     }
+    private void FOVCheck()
+    {
+        Collider[] targetsInFOV = Physics.OverlapSphere(transform.position, radius, targetMask);
+        if (targetsInFOV.Length != 0)
+        {
+            foreach (Collider c in targetsInFOV)
+            {
+                if (c.CompareTag("Player"))
+                {
+                    Vector3 directionToTarget = (c.transform.position - transform.position).normalized;
 
+                    if (Vector3.Angle(transform.forward, directionToTarget) < fovAngle / 2)
+                    {
+                        float distanceToTarget = Vector3.Distance(transform.position, c.transform.position);
+                        if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                        {
+                            playerInFOV = true;
+                        }
+                        else
+                            playerInFOV = false;
+                    }
+                    else
+                        playerInFOV = false;
+                }   
+            }
+        }
+        else if (playerInFOV)
+            playerInFOV = false;
+    }
+  
     private void UpdateAlertstate(bool playerinFOV)
     {
         switch (alertStage)
         {
             case AlertStage.Peaceful:
-                if (playerinFOV)               
-                    alertStage = AlertStage.Intrigued;             
+                alertLevel = 0;
+                if (playerinFOV)
+                    alertStage = AlertStage.Intrigued;
                 break;
 
             case AlertStage.Intrigued:
                 if (playerinFOV)
                 {
-                    alertLevel ++ ;
+                    alertLevel+= alertSpeed * Time.deltaTime;
                     if (alertLevel >= maxAlert)
                     {
                         alertStage = AlertStage.Alerted;
@@ -87,7 +128,7 @@ public class RobotManager : MonoBehaviour
                 }
                 else
                 {
-                    alertLevel--;
+                    alertLevel-= forgetSpeed * Time.deltaTime;
                     if (alertLevel <= 0)
                     {
                         alertStage = AlertStage.Peaceful;
@@ -100,24 +141,21 @@ public class RobotManager : MonoBehaviour
                 {
                     alertStage = AlertStage.Intrigued;
                 }
+                else
+                    alertLevel = 100;
                 break;
-           
+
         }
     }
-
-    private void RobotMovement(Collider[] colliders)
+    private void RobotMovement(GameObject target)
     {
         if (alertStage == AlertStage.Alerted)
         {
-            foreach(Collider c in colliders)
-            {
-                var toPlayer = c.transform.position - transform.position;
-                transform.rotation = Quaternion.RotateTowards(transform.rotation,
-                    Quaternion.LookRotation(toPlayer),
-                    Time.deltaTime * turnRate);
-                transform.Translate(transform.forward * speed * Time.deltaTime);
-
-            }
+            var toPlayer = target.transform.position - transform.position;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                Quaternion.LookRotation(toPlayer),
+                Time.deltaTime * turnRate);
+           // transform.Translate(transform.forward * speed * Time.deltaTime);
         }
     }
 }
--- Whitebox setup/Assets/Scripts/RobotManagerEditor.cs
+++ Whitebox setup/Assets/Scripts/RobotManagerEditor.cs
@@ -6,32 +6,38 @@ using UnityEditor;
 [CustomEditor(typeof(RobotManager))]
 public class RobotManagerEditor : Editor
 {
- private void OnSceneGUI()
+    private void OnSceneGUI()
     {
-        RobotManager robot = (RobotManager)target;
+        RobotManager fov = (RobotManager)target;
+
+        //drawing circle with radius of viewing radius
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+
+        //setting color to alertness level and drawing vision cone
         Color c = Color.green;
-        if(robot.alertStage == AlertStage.Intrigued)
+        if (fov.alertStage == AlertStage.Intrigued)
         {
-            c = Color.Lerp(Color.green, Color.red, robot.alertLevel / robot.maximumAlert);
-        } else if (robot.alertStage == AlertStage.Alerted)
+            c = Color.Lerp(Color.yellow, Color.red, fov.alertLevel / fov.maxAlertEdit);
+        }
+        else if (fov.alertStage == AlertStage.Alerted)
         {
             c = Color.red;
         }
+        Handles.color = new Color(c.r, c.g, c.b, 0.1f);
+        Handles.DrawSolidArc(fov.transform.position, fov.transform.up,
+            Quaternion.AngleAxis(-fov.fovAngle / 2f, fov.transform.up) * fov.transform.forward,
+            fov.fovAngle, fov.radius);
 
-        Handles.color = new Color(c.r, c.g, c.b,0.3f);
-        Handles.DrawSolidArc(
-            robot.transform.position,
-            robot.transform.up,
-           Quaternion.AngleAxis(-robot.fovAngle/2f,robot.transform.up)* robot.transform.forward,
-            robot.fovAngle,
-            robot.fov);
-        Handles.color = c;
-        robot.fov = Handles.ScaleValueHandle( 
-            robot.fov,
-            robot.transform.position + robot.transform.forward * robot.fov,
-            robot.transform.rotation,
-            3,
-            Handles.ConeHandleCap, 1
-            );
+        //drawing raycastline 
+        if (fov.playerInFOV)
+        {
+            Handles.color = Color.cyan;
+            Handles.DrawLine(fov.transform.position, fov.targetRef.transform.position);
+        }
+
+
+
     }
 }
+  
