9a4cdff71d49e351abd32d436da45abac428b38f
IBS-AFICIONADO <j.dezwart@student.utwente.nl>
2023-11-02 13:27:39 +0100

    trying to generate waypoints for patrolling when losing player

--- WhiteboxSetup/Assets/Scripts/RobotManager.cs
+++ WhiteboxSetup/Assets/Scripts/RobotManager.cs
@@ -7,34 +7,37 @@ using UnityEngine.AI;
 public enum AlertStage
 {
     Peaceful,
-    Intrigued,
+    IntriguedL1,
+    IntriguedL2,
     Alerted
 
 }
 
 public class RobotManager : MonoBehaviour
 {
+    [Header("vision and hearing")]
     //parameters vision
     public float visionRadius;
     [Range(0, 360)] public float fovAngle;
-
+    //hearing + powerup stuff
+    public float hearingRadius;
     //parameters before enum change
     const float maxAlert = 100f;
-    public float alertSpeed;
-    public float forgetSpeed;
-
-    public bool visioncone;
-
-
-    public AlertStage alertStage;
-    [Range(0, maxAlert)] public float alertLevel;
-
-    //parametrs for raycasting, detecting objects
     [SerializeField]
     private LayerMask targetMask;
     [SerializeField]
     private LayerMask obstructionMask;
 
+    [Header("alerting and forgetting")]
+    public float alertSpeed;
+    public float forgetSpeed;
+    public AlertStage alertStage;
+    [Range(0, maxAlert)] 
+    public float alertLevel;
+
+    //parametrs for raycasting, detecting objects
+
+    [Header("visioncone settings")]
     //parameters for drawing vision cone
     [SerializeField]
     private float meshResolution;
@@ -45,6 +48,7 @@ public class RobotManager : MonoBehaviour
     private int edgeDetectIte;
     [SerializeField]
     private float edgeDistThreshold;
+    public bool visioncone;
 
     //parameters needed in the editor but not important for changing in unity inspector
     [HideInInspector]
@@ -55,22 +59,28 @@ public class RobotManager : MonoBehaviour
     public Collider targetRef;
     
 
-    //moving AI bits
-    NavMeshAgent agent;
-    private Vector3 player;
+   
 
-    //hearing + powerup stuff
-    public float hearingRadius;
+    
 
     //variables for getting hit with stun
+    [Header("stun settings")]
     [SerializeField]
     private float stunTime;
     private bool isStunned;
     private float initialRadius;
-
+    
+    [Header("AI pathfinding")]
     //For the waypoint
     public Transform[] waypoints;
     int m_CurrentWaypointIndex;
+    public float alertVel;
+    public float intrigueVel;
+    public bool wasChasing;
+
+    //moving AI bits
+    public NavMeshAgent agent;
+    private Vector3 player;
 
     private void Start()
     {
@@ -204,17 +214,37 @@ public class RobotManager : MonoBehaviour
 
   
 
-    private void UpdateAlertstate(bool playerinFOV)
+    public void UpdateAlertstate(bool playerinFOV)
     {
         switch (alertStage)
         {
             case AlertStage.Peaceful:
                 alertLevel = 0;
                 if (playerinFOV)
-                    alertStage = AlertStage.Intrigued;
+                    alertStage = AlertStage.IntriguedL1;
                 break;
 
-            case AlertStage.Intrigued:
+            case AlertStage.IntriguedL1:
+                wasChasing = false;
+                if (playerinFOV)
+                {
+                    alertLevel += alertSpeed;
+                    if (alertLevel >= maxAlert/4)
+                    {
+                        alertStage = AlertStage.IntriguedL2;
+                    }
+                }
+                else
+                {
+                    alertLevel -= forgetSpeed;
+                    if (alertLevel <= 0)
+                    {
+                        alertStage = AlertStage.Peaceful;
+                    }
+                }
+                break;
+
+            case AlertStage.IntriguedL2:
                 if (playerinFOV)
                 {
                     alertLevel += alertSpeed;
@@ -228,8 +258,8 @@ public class RobotManager : MonoBehaviour
                     alertLevel -= forgetSpeed;
                     if (alertLevel <= 0)
                     {
-                        //agent.SetDestination((waypoints[m_CurrentWaypointIndex].position));
-                        alertStage = AlertStage.Peaceful;
+                        wasChasing = true;
+                        alertStage = AlertStage.IntriguedL1;
                     }
                 }
                 break;
@@ -237,7 +267,7 @@ public class RobotManager : MonoBehaviour
             case AlertStage.Alerted:
                 if (!playerinFOV)
                 {
-                    alertStage = AlertStage.Intrigued;
+                    alertStage = AlertStage.IntriguedL2;
                 }
                 else
                     alertLevel = 100;
@@ -253,21 +283,23 @@ public class RobotManager : MonoBehaviour
         {
             if (alertStage == AlertStage.Alerted)
             {
+                agent.speed = alertVel;
                 player = targetRef.transform.position;
                 agent.SetDestination(player);
             }
-            else if (alertStage == AlertStage.Intrigued && !isStunned)
+            else if (alertStage == AlertStage.IntriguedL2 && !isStunned)
             {
+                agent.speed = intrigueVel;
                 player = targetRef.transform.position;
                 agent.SetDestination(player);
             }
             else if (alertStage == AlertStage.Peaceful)
 
-                if (agent.remainingDistance < agent.stoppingDistance)
-                {
-                    m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-                    agent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-                }
+            if (agent.remainingDistance < agent.stoppingDistance)
+            {
+                m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+                agent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+            }
         }
     }
    
@@ -282,7 +314,7 @@ public class RobotManager : MonoBehaviour
         ViewCastInfo oldCast = new ViewCastInfo();
         for (int i = 0; i <= stepCount; i++){
             float angle = transform.eulerAngles.y - fovAngle / 2 + stepAngleSize*i ;
-            //every iteration see what viewcast returns for the fiven segment of the mesh
+            //every iteration see what viewcast returns for the given segment of the mesh
             ViewCastInfo newViewCast = ViewCast(angle);
             if (i > 0)
             {
